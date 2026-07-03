a968a459cc8395ceed9cf5067171d1df0a3a002e
Dolphinman980 <130254302+Dolphinman980@users.noreply.github.com>
2026-07-03 14:49:48 +1000

    Bunch of temporary art assets, basic building scene
    Bunch of temporary art assets, basic building scene

---
+++ Scripts/Global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var ant_count: float = 0
+var spider_count: float = 0
+var aphid_count: float = 0
+var honeydew_count: float = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
