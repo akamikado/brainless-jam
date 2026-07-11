c9dd4babaeb34030661437d68e0815b023bf569f
doshicshow <doshic.show1@gmail.com>
2026-07-11 17:07:14 +0300

    clean up of redundant methods and an if statement

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -102,13 +102,3 @@ func remove_pebbles(count: int) -> bool:
 		pebbles = pebbles - count
 		return true
 	return false
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
--- Scripts/selected_building.gd
+++ Scripts/selected_building.gd
@@ -8,16 +8,17 @@ func _ready() -> void:
 	visible = false
 
 func _input(event: InputEvent) -> void:
-	if visible and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+	if visible and event.is_action_pressed("dragging_building"):
 		place_building.emit(global_position)
 		
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if is_visible_in_tree():
 		global_position = get_global_mouse_position().snapped(Vector2(16,16))
 
 func _on_building_selection_area_selected_building(type: String) -> void:
 	if type == "hidden":
 		visible = false
+		print("hidden")
 	elif type == "ant_hatchery":
 		visible = true
 		texture = ant_hatchery_texture
