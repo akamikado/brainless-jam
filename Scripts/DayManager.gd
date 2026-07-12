110f837ebd635a81f161dae72f710c5ff1bbfac4
Nandan Jadhav <nandanjadhav08@gmail.com>
2026-07-12 18:43:20 -0400

    Updated weather & day manager scripts

--- Scripts/DayManager.gd
+++ Scripts/DayManager.gd
@@ -1,11 +1,72 @@
 extends Node
 
+var current_day: int = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var resource_manager: Node
+@export var weather_manager: Node
 
+var food_quota: int = 5
+var silk_quota: int = 2
+var sticks_quota: int = 2
+var pebbles_quota: int = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func end_day():
+	print("Ending Day ", current_day)
+
+	# 1. Apply building/resource generation here later
+	# BuildingManager.generate_all_resources()
+
+	# 2. Apply weather effects
+	weather_manager.apply_current_weather(resource_manager)
+
+	# 3. Pay quota
+	var survived = pay_daily_quota()
+
+	if survived == false:
+		#game_over()
+		return
+
+# 4. Increase difficulty
+	increase_quota()
+
+	# 5. Move to next day
+	current_day += 1
+	weather_manager.roll_next_weather(current_day)
+
+	print("Starting Day ", current_day)
+	
+func pay_daily_quota() -> bool:
+	if resource_manager.food < food_quota:
+		print("Not enough food!")
+		return false
+	
+	if resource_manager.silk < silk_quota:
+		print("Not enough silk!")
+		return false
+	
+	if resource_manager.sticks < sticks_quota:
+		print("Not enough sticks!")
+		return false
+	
+	if resource_manager.pebbles < pebbles_quota:
+		print("Not enough pebbles!")
+		return false
+
+	resource_manager.remove_food(food_quota)
+	resource_manager.remove_silk(silk_quota)
+	resource_manager.remove_sticks(sticks_quota)
+	resource_manager.remove_pebbles(pebbles_quota)
+
+	return true
+	
+func increase_quota():
+	food_quota += 2
+
+	if current_day % 2 == 0:
+		sticks_quota += 1
+
+	if current_day % 3 == 0:
+		silk_quota += 1
+
+	if current_day % 4 == 0:
+		pebbles_quota += 1
--- Scripts/WeatherManager.gd
+++ Scripts/WeatherManager.gd
@@ -1,11 +1,48 @@
 extends Node
 
+var current_weather: String = "Clear"
+var next_weather: String = "Clear"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var weather_options = [
+	"Clear",
+	"Rain",
+	"Drought",
+	"Wind",
+	"Cold",
+	"Predator Raid"
+]
 
+func _ready():
+	roll_next_weather(1)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func roll_next_weather(day: int):
+	current_weather = next_weather
+	next_weather = weather_options.pick_random()
+
+	print("Current Weather: ", current_weather)
+	print("Tomorrow's Weather: ", next_weather)
+	
+func apply_current_weather(resource_manager):
+	if current_weather == "Clear":
+		print("Clear weather. No effect.")
+
+	elif current_weather == "Rain":
+		print("Rain: lose 2 sticks.")
+		resource_manager.remove_sticks(2)
+
+	elif current_weather == "Drought":
+		print("Drought: lose 2 food.")
+		resource_manager.remove_food(2)
+
+	elif current_weather == "Wind":
+		print("Wind: lose 1 silk and 1 stick.")
+		resource_manager.remove_silk(1)
+		resource_manager.remove_sticks(1)
+
+	elif current_weather == "Cold":
+		print("Cold: extra food required.")
+		resource_manager.remove_food(3)
+
+	elif current_weather == "Predator Raid":
+		print("Predator Raid: lose 1 citizen.")
+		resource_manager.remove_citizens(1)
