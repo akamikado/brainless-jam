7a9b70871c84bb174e28d825893f4b652150dcec
Dolphinman980 <130254302+Dolphinman980@users.noreply.github.com>
2026-07-13 12:31:46 +1000

    fixed ui

--- Scripts/camera-controls.gd
+++ Scripts/camera-controls.gd
@@ -7,6 +7,13 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+func zoom_at(pos: Vector2, scale: float) -> void:
+	zoom.x = scale
+	zoom.y = scale
+	var movement = pos - get_global_mouse_position()
+	offset += movement
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_W):
@@ -17,5 +24,15 @@ func _process(delta: float) -> void:
 		offset.x -= camera_speed
 	if Input.is_key_pressed(KEY_D):
 		offset.x += camera_speed
-	
 	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				var zoom_pos = get_global_mouse_position()
+				zoom_at(zoom_pos, zoom.x * 1.2)
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				var zoom_pos = get_global_mouse_position()
+				zoom_at(zoom_pos, zoom.x * 0.8)
+				
