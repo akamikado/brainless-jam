2bd23f0cace2ac15194023d67ca8b50fd4603fb2
akamikado <manishvasireddy1234@gmail.com>
2026-07-13 08:31:13 +0530

    minor changes to camera and building selection area

--- Scripts/camera-controls.gd
+++ Scripts/camera-controls.gd
@@ -1,21 +1,30 @@
 extends Camera2D
 
 @export var camera_speed = 3
+@export var zoom_speed = 0.2
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_W):
-		offset.y -= camera_speed
-	if Input.is_key_pressed(KEY_S):
-		offset.y += camera_speed
-	if Input.is_key_pressed(KEY_A):
-		offset.x -= camera_speed
-	if Input.is_key_pressed(KEY_D):
-		offset.x += camera_speed
-	
 	pass
+
+func _process(delta: float) -> void:
+	pass
+	
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		match event.keycode:
+			KEY_W, KEY_UP:
+				offset.y -= camera_speed
+			KEY_A, KEY_LEFT:
+				offset.x -= camera_speed
+			KEY_S, KEY_DOWN:
+				offset.y += camera_speed
+			KEY_D, KEY_RIGHT:
+				offset.x += camera_speed
+	if event is InputEventMouseButton and event.pressed:
+		match event.button_index:
+			MOUSE_BUTTON_WHEEL_UP:
+				camera_speed = max(3, camera_speed - 5)
+				zoom += Vector2(zoom_speed, zoom_speed)
+			MOUSE_BUTTON_WHEEL_DOWN:
+				camera_speed += 5
+				zoom -= Vector2(zoom_speed, zoom_speed)
