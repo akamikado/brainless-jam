3e722743236e56e797966023e101d22ccb84896d
doshicshow <doshic.show1@gmail.com>
2026-07-14 19:40:34 +0300

    added right click drag camera controls

--- Scripts/camera-controls.gd
+++ Scripts/camera-controls.gd
@@ -1,19 +1,36 @@
 extends Camera2D
 
-@export var camera_speed := 1000
+@export var keyboard_speed := 1000
+@export var mouse_speed := 2
+
+
+var mouse_velocity: Vector2
 
 func _process(delta: float) -> void:
-	var direction = Input.get_vector("left", "right", "up", "down")
-	global_position = get_screen_center_position() + direction.normalized() * camera_speed * delta
+	var keyboard_direction := Input.get_vector("left", "right", "up", "down")
 	
+	if keyboard_direction != Vector2.ZERO:
+		global_position = get_screen_center_position() + keyboard_direction.normalized() * keyboard_speed * delta
+		
+	if Input.is_action_pressed("mouse_right"):
+		if mouse_velocity != Vector2.ZERO:
+			global_position = get_screen_center_position() + mouse_velocity * mouse_speed
+			
+	mouse_velocity = Vector2.ZERO
+
+
 func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		mouse_velocity = -event.relative
+	
+	# zoom stuff
 	if event.is_action_pressed("zoom_out"):
-		camera_speed += 5
+		keyboard_speed += 5
 		var zoom_pos = get_global_mouse_position()
 		zoom_at(zoom_pos, zoom.x * 0.8)
 	elif event.is_action_pressed("zoom_in"):
 		var zoom_pos = get_global_mouse_position()
-		camera_speed = max(3, camera_speed - 5)
+		keyboard_speed = max(3, keyboard_speed - 5)
 		zoom_at(zoom_pos, zoom.x * 1.2)
 
 
