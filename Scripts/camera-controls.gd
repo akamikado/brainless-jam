7cde4f2a7016aa653f69a2da5d7156affebbc366
doshicshow <doshic.show1@gmail.com>
2026-07-14 18:57:22 +0300

    changed the camera controls

--- Scripts/camera-controls.gd
+++ Scripts/camera-controls.gd
@@ -1,39 +1,25 @@
 extends Camera2D
 
-@export var camera_speed = 3
-
-func _ready() -> void:
-	position = Vector2(600,600)
-	zoom = Vector2(0.5, 0.5)
-	pass
+@export var camera_speed := 1000
 
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_W):
-		offset.y -= camera_speed
-	if Input.is_key_pressed(KEY_S):
-		offset.y += camera_speed
-	if Input.is_key_pressed(KEY_A):
-		offset.x -= camera_speed
-	if Input.is_key_pressed(KEY_D):
-		offset.x += camera_speed
-	
-	pass
+	var direction = Input.get_vector("left", "right", "up", "down")
+	global_position = get_screen_center_position() + direction.normalized() * camera_speed * delta
 	
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		match event.button_index:
-			MOUSE_BUTTON_WHEEL_UP:
-				camera_speed = max(3, camera_speed - 5)
-				var zoom_pos = get_global_mouse_position()
-				zoom_at(zoom_pos, zoom.x * 1.2)
-			MOUSE_BUTTON_WHEEL_DOWN:
-				camera_speed += 5
-				var zoom_pos = get_global_mouse_position()
-				zoom_at(zoom_pos, zoom.x * 0.8)
+	if event.is_action_pressed("zoom_out"):
+		camera_speed += 5
+		var zoom_pos = get_global_mouse_position()
+		zoom_at(zoom_pos, zoom.x * 0.8)
+	elif event.is_action_pressed("zoom_in"):
+		var zoom_pos = get_global_mouse_position()
+		camera_speed = max(3, camera_speed - 5)
+		zoom_at(zoom_pos, zoom.x * 1.2)
 
 
 func zoom_at(pos: Vector2, scale: float) -> void:
-	zoom.x = scale
-	zoom.y = scale
+	# max zoom is around 0.191 i think
+	zoom.x = clampf(scale, 0.19, 1)
+	zoom.y = clampf(scale, 0.19, 1)
 	var movement = pos - get_global_mouse_position()
-	offset += movement
+	global_position += movement
